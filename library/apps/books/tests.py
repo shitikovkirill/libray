73d1d2d8e2554984430ac7cbf109c81d6a644323
Shitikov Kirill <shitikovkirillt@gmail.com>
2025-02-17 22:03:55 +0100

    Fix test

--- library/apps/books/tests.py
+++ library/apps/books/tests.py
@@ -17,6 +17,18 @@ class BookTestCase(APITestCase):
         response = self.client.post(url, data, format="json")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.test_user_token = response.data["access"]
+        
+        user = get_user_model().objects.create_superuser(
+            username="user2", email="user2@mial.com"
+        )
+        user.set_password(password)
+        user.save()
+
+        data = {"username": user.username, "password": password}
+
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.test_admin_user_token = response.data["access"]
 
     def test_create_book_without_autentification(self):
         url = reverse("books:book-list")
@@ -100,21 +112,6 @@ class BookTestCase(APITestCase):
         )
 
     def test_create_book_with_autentification_admin(self):
-        password = "strongpass1"
-        user = get_user_model().objects.create(
-            username="user2", email="user2@mial.com", is_superuser=True
-        )
-
-        user.set_password(password)
-        user.save()
-
-        url = reverse("accounts:token_create")
-        data = {"username": "user1", "password": "strongpass1"}
-
-        response = self.client.post(url, data, format="json")
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        test_user_token = response.data["access"]
-
         url = reverse("books:book-list")
         data = {
             "title": "Book titlr",
@@ -124,13 +121,10 @@ class BookTestCase(APITestCase):
             "published_date": "2025-02-16",
         }
 
-        self.client.credentials(HTTP_AUTHORIZATION="Bearer " + test_user_token)
+        self.client.credentials(HTTP_AUTHORIZATION="Bearer " + self.test_admin_user_token)
         response = self.client.post(
             url,
             data,
         )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertIn(
-            "You do not have permission to perform this action.",
-            response.data["detail"],
-        )
+        self.assertIsNotNone(response.data["id"])
